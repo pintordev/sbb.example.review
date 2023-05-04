2e1cd21999cb203a0e752c23e3c6e5dddb747752
pintordev <pintor.dev@gmail.com>
2023-05-04 20:45:24 +0900

    Jump2SB review til 2.10

--- src/main/java/com/example/sbb/example/review/question/QuestionController.java
+++ src/main/java/com/example/sbb/example/review/question/QuestionController.java
@@ -4,21 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question") // QuestionController로 들어오는 모든 요청에 대한 접두 URL setting?
 @RequiredArgsConstructor // 생성자 자동 생성 애너테이션
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list") // 실제 URL => /question/list
     public String list(Model model) { // Model 객체를 이용하여 controller 상의 데이터를 template에 전달 가능
         // question list를 template에 전달
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList); // questionList 데이터를 "questionList" 라는 이름으로 template에 전달
         return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}") // 실제 URL => /question/detail/#
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/example/sbb/example/review/question/QuestionService.java
+++ src/main/java/com/example/sbb/example/review/question/QuestionService.java
@@ -1,9 +1,12 @@
 package com.example.sbb.example.review.question;
 
+import com.example.sbb.example.review.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.zip.DataFormatException;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> oq = this.questionRepository.findById(id);
+        if (oq.isPresent()) {
+            return oq.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
